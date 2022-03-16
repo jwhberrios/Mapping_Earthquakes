e471f7bb50d2f0686004b3b033f6fdd02cd43d67
jwhberrios <94324519+jwhberrios@users.noreply.github.com>
2022-03-15 23:01:07 -0400

    copied files from single points branch

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -9,11 +9,25 @@ var map = L.map("mapid", {
     zoom: 2
 });
 
+//  Add a marker to the map for Los Angeles, California.
+//L.circle([34.0522, -118.2437], {
+    //radius: 100
+ //}).addTo(map);
+
+// **Skill Drill** create a light-yellow circle with black lines indicating a 300-meter radius of Central Los Angeles.
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color:"black",
+    fillColor: "#ffffa1"
+ }).addTo(map);
+
+// change id: in tile layer below to 'dark-v10'. 
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY,
