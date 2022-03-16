f79ffcdda24a73aa7297fab0e2340915f1291994
jwhberrios <94324519+jwhberrios@users.noreply.github.com>
2022-03-15 23:42:38 -0400

    added circle markers with color relative to population density

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -14,14 +14,34 @@ var map = L.map("mapid", {
     //radius: 100
  //}).addTo(map);
 
-// **Skill Drill** create a light-yellow circle with black lines indicating a 300-meter radius of Central Los Angeles.
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color:"black",
-    fillColor: "#ffffa1"
- }).addTo(map);
+// Get data from cities.js
+let cityData = cities;
 
-// change id: in tile layer below to 'dark-v10'. 
+// Loop through the cities array and create circle radius representative of population for each city.
+//cityData.forEach(function(city) {
+    //console.log(city)
+    //L.circleMarker(city.location, {
+        //radius: city.population/100000
+    //})
+    //add bindPopup to retrieve city name & population with thousands separator "toLocaleString()" added.
+    //.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  //.addTo(map);
+//});
+
+// Skill Drill: create an orange circle popup marker for each city, 
+// with a lineweight of 4, a radius where the population number is decreased by 200,000,  
+// that's on a dark map.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: 'orange'
+    })
+    //add bindPopup to retrieve city name & population with thousands separator "toLocaleString()" added.
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -32,6 +52,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     zoomOffset: -1,
     accessToken: API_KEY,
 }).addTo(map);
+
 
 // Then we add our 'graymap' tile layer to the map.
     streets.addTo(map);
